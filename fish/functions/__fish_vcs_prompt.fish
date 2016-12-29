237b24be5c36c63c4493c48c814e499fb2eb822d
Dave Nuñez <dnunez24@gmail.com>
2016-12-29 09:53:14 -0800

    Pass ARGV to git prompt fish function

--- fish/functions/__fish_vcs_prompt.fish
+++ fish/functions/__fish_vcs_prompt.fish
@@ -1,6 +1,6 @@
 # __fish_vcs_prompt is a function with definition
 function __fish_vcs_prompt --description 'Print the prompts for all available vcsen'
-    __fish_git_prompt " [%s]"
+    __fish_git_prompt $argv
     __fish_hg_prompt
     __fish_svn_prompt
 end
