7bfde4f289e5e50dcd2b651ef41fdc96b9ece7f0
Dave Nuñez <dnunez24@gmail.com>
2016-12-28 21:09:59 -0800

    Fix incorrect color name for Hyper

--- .hyper.js
+++ .hyper.js
@@ -50,7 +50,7 @@ module.exports = {
       green: '#98971a',
       yellow: '#d79921',
       blue: '#458588',
-      magento: '#b16286',
+      magenta: '#b16286',
       cyan: '#689d6a',
       white: '#a89984',
       lightBlack: '#928374',
