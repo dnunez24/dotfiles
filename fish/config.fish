b7869c70eef7f77c8a2f10d22f75a1c08e334461
Dave Nuñez <dnunez24@gmail.com>
2016-12-29 10:31:03 -0800

    Move fish git config to separate file

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,5 @@
 ### Configure fish ###
 
-# Configure git prompt
-set __fish_git_prompt_show_informative_status yes
-set __fish_git_prompt_showuntrackedfiles yes
-set __fish_git_prompt_showdirtystate yes
-set __fish_git_prompt_showstashstate yes
-set __fish_git_prompt_showcolorhints yes
-set __fish_git_prompt_char_stateseparator ' '
-set __fish_git_prompt_char_upstream_prefix 'u'
-
 # Force 256 color mode for Hyper
 set fish_term256 1
 
@@ -21,7 +12,7 @@ set fish_cursor_insert line
 set fish_cursor_visual block
 
 # use vi mode keybindings
-set fish_key_bindings fish_vi_key_bindings 
+set fish_key_bindings fish_vi_key_bindings
 
 # Set pwd length to 1 character, e.g. ~/a/b/c/dir>
 set -U fish_prompt_pwd_dir_length 1
