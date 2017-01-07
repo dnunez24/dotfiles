fc3c3478452a1b18ad7a247ab812c82aae5c0231
Dave Nuñez <dnunez24@gmail.com>
2017-01-07 08:40:16 -0800

    Remove rbenv

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -6,8 +6,3 @@ set -x LANG en_US.UTF-8
 set -x LANGUAGE en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
-
-if which rbenv > /dev/null
-  set -x PATH $HOME/.rbenv/shims $PATH
-  rbenv rehash >/dev/null ^&1
-end
