1b7ff8195da66ced397ac0435a806ba2d3f2ec09
Dave Nuñez <dnunez24@gmail.com>
2017-01-14 15:47:27 -0800

    Use sudo to install bats in TravisCI

--- test/install-dependencies.sh
+++ test/install-dependencies.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 case "$(uname)" in
   Darwin )
     brew update
     brew install bats
     ;;
   * )
-    git clone https://github.com/sstephenson/bats.git /usr/local/src/bats
+    sudo git clone https://github.com/sstephenson/bats.git /usr/local/src/bats
     cd /usr/local/src/bats
     ./install.sh /usr/local
     ;;
