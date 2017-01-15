e64ef81263df19e1667ca624c22ae3a449f089f4
Dave Nuñez <dnunez24@gmail.com>
2017-01-14 16:08:45 -0800

    Check OS name using env variable

--- test/install-dependencies.sh
+++ test/install-dependencies.sh
@@ -2,14 +2,14 @@
 
 set -ex
 
-case "$(uname)" in
-  Darwin )
+case "$TRAVIS_OS_NAME" in
+  osx )
     brew update
     brew install bats
     ;;
   * )
     wget https://github.com/sstephenson/bats/archive/v0.4.0.tar.gz -O /tmp/bats.tar.gz
     tar -xvf /tmp/bats.tar.gz
-    export PATH=$PATH:$PWD/bats-0.4.0/bin/
+    export PATH=$PATH:$PWD/bats-0.4.0/bin
     ;;
 esac
