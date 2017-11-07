f979af69d043650818cccfe9c07014f9441a300d
Dave Nunez <david.nunez@alumni.stanford.edu>
2017-11-07 13:47:47 -0800

    Modify fish config for use with fisher plugins

--- conf/shell/fish/config.fish
+++ conf/shell/fish/config.fish
@@ -2,9 +2,6 @@
 
 # do things that only make sense for an interactive shell here
 if status --is-interactive
-  # Set greeting to null
-  set fish_greeting
-
   # use vi mode keybindings
   set fish_key_bindings fish_vi_key_bindings
 
@@ -12,17 +9,11 @@ if status --is-interactive
   set fish_cursor_default block
   set fish_cursor_insert line
   set fish_cursor_visual block
-
-  # Set pwd length to 1 character, e.g. ~/a/b/c/dir>
-  set -U fish_prompt_pwd_dir_length 1
-
-  # Force 256 color mode for Hyper
-  set fish_term256 1
 end
 
 # Set custom bin directories in PATH
 set fish_user_paths "$HOME/.bin"
-set fish_user_paths $fish_user_paths "(brew --prefix homebrew/php/php71)/bin"
+set fish_user_paths $fish_user_paths (brew --prefix homebrew/php/php71)/bin
 set fish_user_paths $fish_user_paths "./vendor/bin"
 set fish_user_paths $fish_user_paths "./node_modules/.bin"
 set fish_user_paths $fish_user_paths "/usr/local/sbin"
@@ -34,21 +25,23 @@ if test -e "/Applications/LilyPond.app"
   set fish_user_paths $fish_user_paths "/Applications/LilyPond.app/Contents/Resources/bin"
 end
 
-# eval (gpg-agent --daemon --enable-ssh-support)
 # Configure GPG agent
 set -x GPG_TTY (tty)
 set -e SSH_AGENT_PID
+set -q gnupg_SSH_AUTH_SOCK_by; or set gnupg_SSH_AUTH_SOCK_by 0
 
-if "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne %self
- set -x SSH_AUTH_SOCK="(gpgconf --list-dirs agent-ssh-socket)"
+if test "$gnupg_SSH_AUTH_SOCK_by" -ne %self
+  set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 end
-  
-# [ -f ~/.gpg-agent-info ]; and source ~/.gpg-agent-info
-# 
-# if [ -S "$GPG_AGENT_INFO%%" ]
-#     set -x GPG_AGENT_INFO
-#     set -x SSH_AUTH_SOCK
-#     set -x SSH_AGENT_PID
-# else
-#     eval (gpg-agent --daemon ~/.gpg-agent-info)
-# end
+
+set -x EDITOR "atom -w"
+set -x NVM_DIR "$HOME/.nvm"
+set -x COMPOSER_HOME "$HOME/.composer"
+
+# Fixes for Ruby JSON parser throwing errors from Chef gems
+set -x LANG en_US.UTF-8
+set -x LANGUAGE en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+
+
