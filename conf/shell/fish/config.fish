fc99cf506dc1f4dc26fe59d5b2060a590a3bf989
Dave Nunez <david.nunez@alumni.stanford.edu>
2017-11-07 13:51:06 -0800

    Cleanup fish config file

--- conf/shell/fish/config.fish
+++ conf/shell/fish/config.fish
@@ -1,5 +1,3 @@
-### Configure fish ###
-
 # do things that only make sense for an interactive shell here
 if status --is-interactive
   # use vi mode keybindings
@@ -11,13 +9,25 @@ if status --is-interactive
   set fish_cursor_visual block
 end
 
+# Set default editor to Atom
+set -x EDITOR "atom -w"
+
+# Set composer home directory
+set -x COMPOSER_HOME "$HOME/.composer"
+
+# Fixes for Ruby JSON parser throwing errors from Chef gems
+set -x LANG en_US.UTF-8
+set -x LANGUAGE en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+
 # Set custom bin directories in PATH
 set fish_user_paths "$HOME/.bin"
 set fish_user_paths $fish_user_paths (brew --prefix homebrew/php/php71)/bin
 set fish_user_paths $fish_user_paths "./vendor/bin"
 set fish_user_paths $fish_user_paths "./node_modules/.bin"
 set fish_user_paths $fish_user_paths "/usr/local/sbin"
-set fish_user_paths $fish_user_paths "$HOME/.composer/vendor/bin"
+set fish_user_paths $fish_user_paths "$COMPOSER_HOME/vendor/bin"
 set fish_user_paths $fish_user_paths "$HOME/.platformsh/bin/"
 set fish_user_paths $fish_user_paths "$HOME/.cargo/bin"
 
@@ -33,15 +43,3 @@ set -q gnupg_SSH_AUTH_SOCK_by; or set gnupg_SSH_AUTH_SOCK_by 0
 if test "$gnupg_SSH_AUTH_SOCK_by" -ne %self
   set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 end
-
-set -x EDITOR "atom -w"
-set -x NVM_DIR "$HOME/.nvm"
-set -x COMPOSER_HOME "$HOME/.composer"
-
-# Fixes for Ruby JSON parser throwing errors from Chef gems
-set -x LANG en_US.UTF-8
-set -x LANGUAGE en_US.UTF-8
-set -x LC_ALL en_US.UTF-8
-set -x LC_CTYPE en_US.UTF-8
-
-
