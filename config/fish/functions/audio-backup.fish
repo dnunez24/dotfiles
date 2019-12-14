010c8bfcf695b61e7c5a8ed4b5c889bdaafdc8a5
Dave Nunez <david.nunez@alumni.stanford.edu>
2019-12-14 10:21:59 -0800

    Add fish functions

---
+++ config/fish/functions/audio-backup.fish
@@ -0,0 +1,11 @@
+function audio-backup --description "Back up audio production files to AWS S3"
+  set -l musicDir "$HOME/Music"
+  set -l s3Bucket "s3://audio-backups"
+  set -l excludes
+
+  for item in "*/.DS_Store" "$musicDir/iTunes/*" "$musicDir/GarageBand/*" "$musicDir/iPod/*" "$musicDir/Spitfire Audio/Settings/.ImageCache/*"
+    set excludes $excludes "--exclude" "$item"
+  end
+
+  aws s3 sync --profile audio-backups $excludes "$musicDir/" "$s3Bucket/"
+end
